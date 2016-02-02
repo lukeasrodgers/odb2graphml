e75201344dc2a4d0e981cdd2402ec1f89a001928
Luke Rodgers <lukeasrodgers@gmail.com>
2016-02-01 22:11:26 -0500

    Refactor success and failure notification.

--- index.js
+++ index.js
@@ -58,19 +58,10 @@ function run(argv) {
 
   return new Promise(function(fulfill, reject) {
     convertFile(config.inputFilePath, config.outputFilePath, config.schemaInfo, config.options).then(function(result) {
-      var edgeCountString = util.pluralize('edge', result.edgeCount);
-      var vertexCountString = util.pluralize('vertex', result.vertexCount, 'vertices');
-      var prunedEdgeCountString = util.pluralize('edge', result.prunedEdgeCount);
-      console.log(`Success! Converted ${edgeCountString} and ${vertexCountString}. Pruned ${prunedEdgeCountString}. Written to ${outputFilePath}`);
+      notifySuccess(result);
       fulfill();
     }, function(e) {
-      console.error('Failed');
-      if (e.code === 'ENOENT') {
-        console.error(`Could not open ${config.inputFilePath} for reading`);
-      }
-      else {
-        console.error(e);
-      }
+      notifyFailure(config, e);
       reject(e);
       if (!process.env.TEST) {
         process.exit(1);
@@ -78,6 +69,23 @@ function run(argv) {
     });
   });
 };
+
+function notifySuccess(result) {
+  var edgeCountString = util.pluralize('edge', result.edgeCount);
+  var vertexCountString = util.pluralize('vertex', result.vertexCount, 'vertices');
+  var prunedEdgeCountString = util.pluralize('edge', result.prunedEdgeCount);
+  console.log(`Success! Converted ${edgeCountString} and ${vertexCountString}. Pruned ${prunedEdgeCountString}. Written to ${outputFilePath}`);
+}
+
+function notifyFailure(config, e) {
+  console.error('Failed');
+  if (e.code === 'ENOENT') {
+    console.error(`Could not open ${config.inputFilePath} for reading`);
+  }
+  else {
+    console.error(e);
+  }
+}
 
 module.exports.run = run;
 
